2ad53cf4aa2d47adebc24b0eb850c4332a67f873
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-10-09 18:05:56 +0530

    Added DB config

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongo_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
