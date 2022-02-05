15018b2bea879700d1401d66271e34604195c659
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-02-05 15:38:27 +0530

    added quiz generating function and dynamic routing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { login, register } = require('../controllers/auth');
+const {genLink,getQuiz} = require("../helper/quiz")
+
 
 router.post('/login', login);
 
 router.post('/',  register);
+
+//for getting quiz
+router
+.route('/quiz/:id')
+.get(getQuiz)
 
 module.exports = router;
